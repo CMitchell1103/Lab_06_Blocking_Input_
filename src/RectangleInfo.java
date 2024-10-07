ac643ce593d229731b41606c9a9fa331769f0431
CMitchell1103 <mitch3cd@mail.uc.edu>
2024-10-07 18:28:49 -0400

    New task 3 commit and push

--- src/RectangleInfo.java
+++ src/RectangleInfo.java
@@ -21,9 +21,11 @@ public class RectangleInfo {
 
         double area = width * height;
         double perimeter = 2 * (width + height);
+        double diagonal = Math.sqrt(width * width + height * height);
 
         System.out.println("The area of the rectangle is " + area);
         System.out.println("The perimeter of the rectangle is " + perimeter);
+        System.out.println("The diagonal of the rectangle is " + diagonal);
     }
 
 
